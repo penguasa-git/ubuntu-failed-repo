4d2bf24dde4386f3ce6ae30b9efc72673e71f0ff
penguasa-git <149644506+penguasa-git@users.noreply.github.com>
2023-11-15 19:12:26 +0800

    Add files via upload

---
+++ gun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Backup existing sources.list files
+sudo mv /etc/apt/sources.list{,.backup}
+sudo mv /etc/apt/sources.list.d{,.backup}
+
+# Create a new sources.list.d directory
+sudo mkdir /etc/apt/sources.list.d
+
+# Copy the example sources.list file to /etc/apt/sources.list
+sudo cp /usr/share/doc/apt/examples/sources.list /etc/apt/sources.list
+
+# Update package information
+sudo apt update
+
+# Add additional repositories
+sudo add-apt-repository restricted
+sudo add-apt-repository multiverse
+sudo add-apt-repository universe
+
+# Update package information again
+sudo apt update
